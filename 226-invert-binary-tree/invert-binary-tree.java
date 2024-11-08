f12377b0b031abf87e6cfcd482941a1d27e32fe7
Yeongseo Na <panda0329@naver.com>
2024-11-08 16:44:49 +0900

    Time: 0 ms (100.00%) | Memory: 41.3 MB (25.98%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.java
+++ 226-invert-binary-tree/invert-binary-tree.java
@@ -19,34 +19,12 @@ class Solution {
             return null;
         }
 
-        List<TreeNode> children = getChildren(root);
-        invertSubtree(root);
-        while(!children.isEmpty()) {
-            List<TreeNode> grandChildren = new LinkedList<>();
-            for(TreeNode node : children) {
-                invertSubtree(node);
-                grandChildren.addAll(getChildren(node));
-            }
-            children = grandChildren;
-        }
-
-        return root;
-    }
-
-    void invertSubtree(TreeNode root) {
         TreeNode tmp = root.left;
         root.left = root.right;
         root.right = tmp;
-    }
 
-    List<TreeNode> getChildren(TreeNode root) {
-        List<TreeNode> children = new LinkedList<>();
-        if(root.left != null) {
-            children.add(root.left);
-        }
-        if(root.right != null) {
-            children.add(root.right);
-        }
-        return children;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
     }
 }
