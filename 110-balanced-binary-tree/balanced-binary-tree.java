0a8063f0454eff1755ef2c500fbe66d28ad888b0
Yeongseo Na <panda0329@naver.com>
2024-11-11 22:31:12 +0900

    Time: 0 ms (100.00%) | Memory: 44 MB (93.31%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,42 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public boolean isBalanced(TreeNode root) {
+        return getSubtreeHeight(root) != -1;
+    }
+
+    // depth of the two subtrees of every node never differs by more than one
+    // compare subtree's height and return it's height or -1
+    static int getSubtreeHeight(TreeNode root) {
         if(root == null) {
-            return true;
+            return 0;
         }
-        return recur(root, true);
+
+        int leftHeight = getSubtreeHeight(root.left);
+        int rightHeight = getSubtreeHeight(root.right);
+
+        if(leftHeight == -1 || rightHeight == -1) {
+            return -1;
+        }
+
+        if(Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
     }
-
-    static int calculateHeight(TreeNode root, int height) {
-        if (root == null) {
-            return height;
-        }
-
-        return Math.max(calculateHeight(root.left, height + 1), calculateHeight(root.right, height + 1));
-    }
-
-    static int countNodes(TreeNode root) {
-        if (root.left == null && root.right == null) {
-            return 1;
-        }
-        if (root.left == null) {
-            return 1 + countNodes(root.right);
-        }
-        if (root.right == null) {
-            return 1 + countNodes(root.left);
-        }
-
-        return 1 + countNodes(root.left) + countNodes(root.right);
-    }
-
-    static boolean recur(TreeNode root, boolean result) {
-        if(root == null || !result) {
-        return result;
-        }
-
-        int left = root.left == null ? 0 : calculateHeight(root.left, 0);
-        int right = root.right == null ? 0 : calculateHeight(root.right, 0);
-
-        boolean newResult = (Math.abs(left - right) <= 1);
-        return recur(root.left, newResult) && recur(root.right, newResult);
-  }
 }
