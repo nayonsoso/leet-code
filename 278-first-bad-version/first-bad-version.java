9dec201104e625224b9f5019a54d4ac9e8e62365
Yeongseo Na <panda0329@naver.com>
2024-11-20 14:33:11 +0900

    Time: 24 ms (98.09%) | Memory: 40.1 MB (69.04%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.java
@@ -0,0 +1,22 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+
+        int middle = 1;
+
+        while(start < end) {
+            middle = start + (end - start) / 2;
+            if (isBadVersion(middle)) {
+                end = middle;
+            } else {
+                start = middle + 1;
+            }
+        }
+
+        return start;
+    }
+}
