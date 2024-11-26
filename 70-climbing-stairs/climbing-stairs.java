1f63bd35c94849221f01c35e6813b0ed024fe9a2
Yeongseo Na <panda0329@naver.com>
2024-11-26 23:32:05 +0900

    Time: 0 ms (100.00%) | Memory: 40.3 MB (55.58%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    int[] arr = new int[46];
+
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+
+        if (arr[n] != 0) {
+            return arr[n];
+        }
+
+        arr[n] = climbStairs(n-1) + climbStairs(n-2);
+        return arr[n];
+    }
+}
