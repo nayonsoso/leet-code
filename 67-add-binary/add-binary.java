edb3f46a845dc7c13c078003fac6a633512e1e37
Yeongseo Na <panda0329@naver.com>
2024-12-06 03:27:23 +0900

    Time: 2 ms (45.19%) | Memory: 42.6 MB (18.78%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -4,59 +4,27 @@ class Solution {
     }
 
     static String addBinary1(String a, String b) {
-        boolean[][] preprocessed = makeLengthSame(a, b);
-        boolean[] a_ = preprocessed[0];
-        boolean[] b_ = preprocessed[1];
+        if (a.length() > b.length()) {
+            b = "0".repeat(a.length() - b.length()) + b;
+        } else if (a.length() < b.length()) {
+            a = "0".repeat(b.length() - a.length()) + a;
+        }
 
-        int idx = a_.length - 1;
-
+        int idx = a.length() - 1;
         boolean carry = false;
-        boolean[] result = new boolean[idx + 2];
+        StringBuilder answer = new StringBuilder();
         while (idx >= 0) {
-            if (a_[idx] == b_[idx]) {
-                result[idx + 1] = carry;
-                carry = a_[idx];
-            } else if (a_[idx] != b_[idx]) {
-                result[idx + 1] = !carry;
-            }
+            int tmp = (a.charAt(idx) - '0') + (b.charAt(idx) - '0');
+            tmp += carry ? 1 : 0; 
+            answer.append(tmp == 2 || tmp == 0 ? "0" : "1");
+            carry = (tmp >= 2 ? true : false);
             idx--;
         }
-        result[0] = carry;
+        if (carry) {
+            answer.append("1");
+        }
+        answer.reverse();
 
-        StringBuilder answer = new StringBuilder();
-        int boundary = 0;
-        if (!result[0]) {
-            boundary = 1;
-        }
-        System.out.print(boundary);
-        System.out.print(result.length);
-        for (int i = boundary; i < result.length; i++) {
-            answer.append(result[i] ? "1" : "0");
-        }
         return answer.toString();
-    }
-
-    static boolean[][] makeLengthSame(String a, String b) {
-        int aLength = a.length();
-        int bLength = b.length();
-        int maxLength = Math.max(aLength, bLength);
-
-        boolean[][] result = new boolean[2][maxLength];
-        if (aLength > bLength) {
-            for (int i = 0; i < aLength; i++) {
-                result[0][i] = (a.charAt(i) == '0' ? false : true);
-            }
-            for (int i = 0; i < bLength; i++) {
-                result[1][aLength - bLength + i] = (b.charAt(i) == '0' ? false : true);
-            }
-        } else {
-            for (int i = 0; i < aLength; i++) {
-                result[0][bLength - aLength + i] = (a.charAt(i) == '0' ? false : true);
-            }
-            for (int i = 0; i < bLength; i++) {
-                result[1][i] = (b.charAt(i) == '0' ? false : true);
-            }
-        }
-        return result;
     }
 }
