31ea952344411dec4c258445209b0ce1974fa5be
Yeongseo Na <panda0329@naver.com>
2025-03-01 04:13:19 +0900

    Time: 0 ms (100.00%) | Memory: 40.2 MB (90.86%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,18 +1,17 @@
 class Solution {
-    public int climbStairs(int n) {
-        int first = 1;
-        int second = 2;
-        int third = 0;
-
-        if (n == 1) return 1;
-        if (n == 2) return 2;
-        
-        // 1, 2, 3, 5, 8, 13
-        for(int i = 0; i < n-2 ; i++) {
-            third = first + second;
-            first = second;
-            second = third;
+    public int climbStairs(int stairCount) {
+        if (stairCount == 1) {
+            return 1;
         }
-        return third;
+
+        int[] dp = new int[stairCount + 1];
+
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= stairCount; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[stairCount];
     }
 }
