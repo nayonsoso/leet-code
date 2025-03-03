9d03fc2c68fd737f28d84966a1d31af1ec8be7d2
Yeongseo Na <panda0329@naver.com>
2025-03-03 19:45:39 +0900

    Time: 0 ms (100.00%) | Memory: 45.8 MB (83.36%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,60 +1,27 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        if (nums.length == 0)
-            return new int[] { -1, -1 };
-        return Solution.findFirstOrLastIdx(nums, target);
-    }
-
-    private static int[] findFirstOrLastIdx(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
-        int[] answer = new int[2];
-
-        while (start <= end) {
-            int middle = (start + end) / 2;
-            if (target < nums[middle]) {
-                end = middle - 1;
-            } else if (nums[middle] < target) {
-                start = middle + 1;
-            } else {
-                answer[0] = Solution.findFirstIdx(nums, middle);
-                answer[1] = Solution.findLastIdx(nums, middle);
-                return answer;
-            }
+    public int[] searchRange(int[] arr, int target) {
+        int leftBound = getLeftBound(arr, target);
+        int firstPosition = leftBound + 1;
+        int lastPosition = getLeftBound(arr, target + 1);
+        if (firstPosition < arr.length && arr[firstPosition] == target) {
+            return new int[] { firstPosition, lastPosition };
         }
-
         return new int[] { -1, -1 };
     }
 
-    private static int findFirstIdx(int[] nums, int any) {
-        int start = -1;
-        int end = any + 1;
-        int target = nums[any];
+    private static int getLeftBound(int[] arr, int target) {
+        int left = -1;
+        int right = arr.length;
 
-        while (start + 1 < end) {
-            int middle = (start + end) / 2;
-            if (nums[middle] < target) {
-                start = middle;
-            } else {
-                end = middle;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < target) {
+                left = mid;
+            } else if (arr[mid] >= target) {
+                right = mid;
             }
         }
-        return end;
-    }
 
-    private static int findLastIdx(int[] nums, int any) {
-        int start = any - 1;
-        int end = nums.length;
-        int target = nums[any];
-
-        while (start + 1 < end) {
-            int middle = (start + end) / 2;
-            if (target < nums[middle]) {
-                end = middle;
-            } else {
-                start = middle;
-            }
-        }
-        return start;
+        return left;
     }
 }
