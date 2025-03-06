3fe651c159810d8237eecebb3057b1723582eb2c
Yeongseo Na <panda0329@naver.com>
2025-03-06 23:29:29 +0900

    Time: 2 ms (75.01%) | Memory: 53.6 MB (14.02%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,14 +1,23 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        int freq = 0;
-        int candidate = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (freq == 0) {
-                candidate = nums[i];
+    public int majorityElement(int[] nums) { // [3]
+
+        // checked the element
+        int count = 0;
+        int recentNum = nums[0]; // 3
+
+        for (int i = 1; i < nums.length; i++) { // 2
+            if (nums[i] == recentNum) { // 3 != 2
+                count++;
+            } else {
+                if (count - 1 < 0) { // -1
+                    recentNum = nums[i]; // recentNum = 3;
+                    count = 0; // count = 0;
+                } else {
+                    count--;
+                }
             }
-            freq += candidate == nums[i] ? 1 : -1;
         }
 
-        return candidate;
+        return recentNum; // 3
     }
 }
